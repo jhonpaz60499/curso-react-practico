dcbd4849fb8d1a1ce58af54ab67d49b06f7d9a9d
Jhon Paz <jfpazguzman@gmail.com>
2025-02-18 15:36:08 -0500

    creacion de las diferentes carpetas con las diferentes paginas que vamos a tener en la aplicacion

---
+++ src/Pages/MyAccount/index.jsx
@@ -0,0 +1,9 @@
+function MyAccount() {
+    return (
+      <div className="bg-red-100">
+        My account
+      </div>
+    )
+  }
+  
+  export default MyAccount
---
+++ src/Pages/MyOrder/index.jsx
@@ -0,0 +1,9 @@
+function MyOrder() {
+    return (
+      <div className="bg-red-100">
+        My order
+      </div>
+    )
+  }
+  
+  export default MyOrder
