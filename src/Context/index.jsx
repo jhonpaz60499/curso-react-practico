08caa1c1ec6a20fbfe3167b2e659e9789b5185e1
Jhon Paz <jfpazguzman@gmail.com>
2025-02-21 14:54:50 -0500

    maquetacion de product detail para que se abra, ojo, solo se abre, falta que se cierre

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -7,14 +7,19 @@ export const ShoppingCartContext = createContext()
 // con este provider vamos a "proveer" toda la informacion a los demas 
 export const ShoppingCartProvider = ({children}) => {
     const [count, setCount] = useState(0)
-
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
 
     return (
         <ShoppingCartContext.Provider value={{ 
             // aqui lo que hacemos es proveer a los hijos (lo que esta abajo que se llama children) con los datos para que ellos los usen y los
             // mofiquen tambien si lo necesitan
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen
         }}>
             {children}
         </ShoppingCartContext.Provider>
