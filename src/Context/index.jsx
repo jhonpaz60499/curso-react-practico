aab82b6b58d038b9bef2e4b29130de0e4f607740
Jhon Paz <jfpazguzman@gmail.com>
2025-02-21 12:33:57 -0500

    implementado el contador de productos en el carrito

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,13 +1,21 @@
 // AQUI VAMOS A CREAR TODO EL CONTEXTO GLOBAL DE APLICACION, DE AQUI SALDRA LA INFORMACION A TODAS PARTES
 // este sera el banco de informacion global
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const ShoppingCartContext = createContext()
+export const ShoppingCartContext = createContext()
 
 // con este provider vamos a "proveer" toda la informacion a los demas 
 export const ShoppingCartProvider = ({children}) => {
+    const [count, setCount] = useState(0)
+
+
     return (
-        <ShoppingCartContext.Provider>
+        <ShoppingCartContext.Provider value={{ 
+            // aqui lo que hacemos es proveer a los hijos (lo que esta abajo que se llama children) con los datos para que ellos los usen y los
+            // mofiquen tambien si lo necesitan
+            count,
+            setCount
+        }}>
             {children}
         </ShoppingCartContext.Provider>
     )
